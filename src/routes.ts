473bd04b2592e261e0249bbd29bdc7147cde8684
wakenedo <a_nuner@outlook.com>
2021-10-20 22:05:56 -0300

    Backend Configuration - Part 4

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,24 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
+import { Get3LastMessagesController } from './controllers/GetLast3MessagesController';
 import { ensureAuthenticated } from './middleware/ensureAuthencticated';
+import { ProfileUserController } from './controllers/ProfileUserController';
+
 
 const router = Router();
 
-router.post("/authenticate", new AuthenticateUserController().handle)
+router.post("/authenticate", new AuthenticateUserController().handle);
 
 router.post("/messages",
  ensureAuthenticated, 
  new CreateMessageController().handle
  );
+
+ router.get("/messages/last3", new Get3LastMessagesController().handle);
+
+ router.get("/profile", ensureAuthenticated , new ProfileUserController().handle)
+
 
 
 export { router };
